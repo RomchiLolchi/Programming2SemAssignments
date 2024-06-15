ba21d53c95daff8c7937890f9814d546ff3babbf
Roman Popov <pri20051982@gmail.com>
2024-06-15 07:02:41 +0300

    Adds binary search task and alters test file for searches

---
+++ src/main/java/search/binary/BinarySearch.java
@@ -0,0 +1,22 @@
+package search.binary;
+
+public class BinarySearch {
+    public int binarySearch(int value, int[] array) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int guess = array[mid];
+            if (guess == value) {
+                return mid;
+            }
+            if (guess > value) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
